527ba74c9bbe2f9e4681864758f0bc0371274a25
sunilselva3335@gmail.com <sunilselva3335@gmail.com>
2021-07-17 15:36:41 +0530

    List all admins --html and api

---
+++ src/main/java/com/example/demo/modal/AdminCustomDetails.java
@@ -0,0 +1,18 @@
+package com.example.demo.modal;
+
+public class AdminCustomDetails {
+
+    private String name;
+
+    public AdminCustomDetails(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
